593b8e196af5aa41f8c191edd59e2b916cc74f49
HengtaoDing774 <56089705+HengtaoDing774@users.noreply.github.com>
2025-12-23 18:31:21 +0800

    Update PGPFMIL.py

--- models/PGPFMIL.py
+++ models/PGPFMIL.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-# from nystrom_attention.nystrom_attention import NystromAttention
+
 
 from math import ceil
 import torch
